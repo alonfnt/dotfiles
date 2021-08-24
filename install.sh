5948c27aae520accfc3264190db998bf16eceaf4
Albert Alonso <alonfnt@pm.me>
2021-08-24 13:51:03 +0200

    fix: add -y arg to install to avoid typing

--- install.sh
+++ install.sh
@@ -4,17 +4,17 @@ set -x
 
 if [ ! -d /usr/share/fonts/ibm-plex-mono-fonts/ ]
 then
-	sudo dnf install ibm-plex-fonts-all
+	sudo dnf install ibm-plex-fonts-all -y
 fi
 
 if [ ! -f /usr/bin/stow ]
 then
-	sudo dnf install stow
+	sudo dnf install stow -y
 fi
 
 if [ ! -f /usr/bin/nvim ]
 then
-	sudo dnf install neovim
+	sudo dnf install neovim -y
 fi
 
 
